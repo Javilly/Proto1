eee134aebc0f0dcb73245d8b3ac04fdf0e86dad8
JAVI\Javier <javi.valda@gmail.com>
2018-01-13 18:58:36 -0300

    Añadí materiales (Colores para las dos esferas y el tablero y scripts de movimiento y de no rotar.

--- Assets/Scripts/MovimientoP2.cs
+++ Assets/Scripts/MovimientoP2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movimiento : MonoBehaviour {
+public class MovimientoP2 : MonoBehaviour {
 
     public float speed;
 
@@ -15,8 +15,8 @@ public class Movimiento : MonoBehaviour {
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal P2");
+        float moveVertical = Input.GetAxis("Vertical P2");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
