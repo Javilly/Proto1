248fe7e2cd4ae356fcf3c218ed5f8c3e6375d8bd
JAVI\Javier <javi.valda@gmail.com>
2018-01-13 17:10:27 -0300

    Creación del proyecto

---
+++ Assets/NoRotar.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoRotar : MonoBehaviour {
+
+    void FixedUpdate() { Vector3 oldRot = transform.rotation.eulerAngles; transform.rotation = Quaternion.Euler(oldRot.x, 90, oldRot.z); }
+}
